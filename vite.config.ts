001eaa926525c13b2924a6ba5317a6b0cc34e153
Zu34 <gmathi091@gmail.com>
2025-11-04 13:55:52 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/Atlas-House/',
+  base: command === 'serve' ? '/' : '/atlas-house0/',
   build: { outDir: 'docs' },
    plugins: [react()]
 }))
